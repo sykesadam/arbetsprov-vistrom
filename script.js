824a7c28fb115e6e162462af0f2580ed767f560a
sykesadam <adam.s-98@hotmail.com>
2020-08-05 16:51:12 +0200

    Finjusteringar
    Lade mer tid på att finjustera sidan. Göra den snyggare responsivt.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const accordionButtons = document.querySelectorAll(".accordion__item");
 accordionButtons.forEach((btn) => {
 	btn.addEventListener("click", () => {
-		accordionButtons.forEach((btn) => btn.classList.remove("show"));
-		if (btn.classList.contains("show")) {
-			btn.classList.remove("show");
-		} else {
+		if (!btn.classList.contains("show")) {
+			accordionButtons.forEach((btn) => btn.classList.remove("show"));
 			btn.classList.add("show");
+		} else {
+			btn.classList.remove("show");
 		}
 	});
 });
@@ -13,13 +13,19 @@ accordionButtons.forEach((btn) => {
 const carouselItems = document.querySelectorAll(".carousel-item");
 carouselItems.forEach((btn, i) => {
 	const carousel = document.querySelector(".product__carousel");
+	let imageWidth = document.querySelector(".product-image").offsetWidth;
 
-	const image = document.querySelector(".product-image").offsetWidth;
+	// för att bilderna ska flyttas helt när skärmen ändras.
+	window.addEventListener(
+		"resize",
+		() =>
+			(imageWidth = document.querySelector(".product-image").offsetWidth)
+	);
 
 	btn.addEventListener("click", () => {
 		carousel.scrollTo({
 			top: 0,
-			left: i * image,
+			left: i * imageWidth,
 			behavior: "smooth",
 		});
 		carouselItems.forEach((btn) => btn.classList.remove("selected"));
